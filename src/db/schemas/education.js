e7fd60a345c586ecb570412916ed8221d1323479
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-04-27 01:15:59 +0900

    (fix) modify schema type, add timestamps

--- src/db/schemas/education.js
+++ src/db/schemas/education.js
@@ -4,25 +4,29 @@ import { Schema, model } from 'mongoose';
 // 졸업타입코드는 숫자로, 필수값
 // 1: 재학중 2: 학사졸업(기본값) 3: 석사졸업 4: 박사졸업
 // 학위, 졸업구분으로 나누는 것이 좋을듯.
-const EducationSchema = new Schema({
-    userId:{
-        type:String,
-        required:true,
+const EducationSchema = new Schema(
+    {
+        userId:{
+            type:String,
+            required:true,
+        },
+        schoolName:{
+            type:String,
+            required:true,
+        },
+        major:{
+            type:String,
+            required:true,
+        },
+        graduationTypeCode:{
+            type:String,
+            required:true,
+        },
     },
-    schoolName:{
-        type:String,
-        required:true,
-    },
-    major:{
-        type:String,
-        required:true,
-    },
-    graduationTypeCode:{
-        type:Number,
-        required:true,
-        default:2,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 const EducationModel = model('Education', EducationSchema);
 
