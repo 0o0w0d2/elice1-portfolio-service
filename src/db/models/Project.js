165333fe8f9e4dfd1962d42ab4ba21303e96c1ab
[AI7] 김지원 <0o0w0d2@gmail.com>
2023-04-27 12:54:57 +0000

    (fix): projectId -> _id

--- src/db/models/Project.js
+++ src/db/models/Project.js
@@ -2,14 +2,13 @@ import { ProjectModel } from '../schemas/project';
 
 
 // 프로젝트 생성
-const createProject = async ({ userId, title, startDate, endDate, description, projectId }) => {
+const createProject = async ({ userId, title, startDate, endDate, description }) => {
     const createdNewProject = await ProjectModel.create({
         userId,
         title,
         startDate, 
         endDate, 
         description, 
-        projectId,
     });
     return createdNewProject;
 };
@@ -20,22 +19,22 @@ const findProjectById = async ( userId ) => {
 };
 
 // projectId를 통해 프로젝트 조회
-const findProjectByProjectId = async (projectId) => {
-    return await ProjectModel.findOne({ projectId: projectId });
+const findProjectByProjectId = async (_id) => {
+    return await ProjectModel.findOne({ _id });
 };
 
 // projectId를 통해 프로젝트 수정             
-const updateProject = async ({ projectId, updateValues }) => {
+const updateProject = async ({ _id, updateValues }) => {
 
-    const updatedProject = await ProjectModel.findOneAndUpdate({ projectId }, 
+    const updatedProject = await ProjectModel.findOneAndUpdate({ _id }, 
         { updateValues });  
     return updatedProject;
 };
 
 // projectId를 통해 프로젝트 삭제
-const deleteProject = async (projectId) => {
+const deleteProject = async (_id) => {
 
-    const deletedProject = await ProjectModel.findOneAndDelete({ projectId });
+    const deletedProject = await ProjectModel.findOneAndDelete({ _id });
            
     return deletedProject;
 };
