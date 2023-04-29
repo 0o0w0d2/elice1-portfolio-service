88d3a528476206cb03e134df0b98cba36d987b51
peter parker <thisisjustpeter@gmail.com>
2023-04-29 20:20:29 +0900

    (refactor): Refactor Award controller, router, and service

--- src/routers/awardRouter.js
+++ src/routers/awardRouter.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-// To integrate middlewares
 import { login_required } from '../middlewares/login_required';
 import { validate_user_award } from '../middlewares/validate_user_resource';
 import {
@@ -11,16 +10,11 @@ import {
 
 const awardRouter = Router();
 
-awardRouter.get(
-  // based on the fact that the current user is already authenticated
-  '/awards', //and endpoints only needs to return the awards that belongs to the current user.
-  login_required,
-  getAllAwards
-);
+awardRouter.get('/awards/:userId', login_required, getAllAwards);
 
 awardRouter.post('/awards', login_required, createNewAward);
 
-awardRouter.put(
+awardRouter.patch(
   '/awards/:id',
   login_required,
   validate_user_award,
