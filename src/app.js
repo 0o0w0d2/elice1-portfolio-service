427c170900116add986a49db47dd123606499a73
[AI7] 김지원 <0o0w0d2@gmail.com>
2023-05-02 18:56:18 +0000

    (feat): comment/ 미완성

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use(educationRouter);
 app.use(awardRouter);
 app.use(projectRouter);
 app.use(certificationRouter);
-app.use(commentRouter)
+
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
 
